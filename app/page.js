168d7ce1e3c456e4b898151c5bd90d7e6e601f96
okithaA <okitha.al@gmail.com>
2025-12-31 15:31:12 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import ComIntro from "./components/ComIntro";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Qualifications from "./components/Qualifications";
-import Project from "./components/Project";
 
 export default function Home() {
   return (
@@ -15,9 +14,6 @@ export default function Home() {
         </section>
         <section className="mt-8">
            <ComIntro />
-        </section>
-        <section className="mt-8">
-           <Project />
         </section>
         <section id='qualifications' className="mt-8">
           <Qualifications />
